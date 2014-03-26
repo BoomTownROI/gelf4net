436c9348508d354609c7a79fc46099f57ab6cd45
Brad Mitchell <bmitchell@boomtownroi.com>
2014-03-26 16:55:49 -0400

    Debug print

--- build.ps1
+++ build.ps1
@@ -5,23 +5,12 @@ param(
     [System.Collections.Hashtable]$properties = @{}
   )
 
-#Write-Host $MyInvocation.MyCommand.Definition
-#Write-Host $MyInvocation.MyCommand.Path
-#Write-Host $MyInvocation.MyCommand.Name
-
-#$root = Split-Path -parent $MyInvocation.MyCommand.Definition
-$root = (Split-Path $script:MyInvocation.MyCommand.Path)
-
-$psversion = $host.Runspace.Version.Major
-
-Write-Host "ps version: $psversion"
+$root = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 Write-Host "root: $root"
 
 Import-Module $root\tools\psake\psake.psm1 -ErrorAction SilentlyContinue
 
+Invoke-psake $root\default.ps1 -properties $properties -taskList $taskList
 
-
-#Invoke-psake $root\default.ps1 -properties $properties -taskList $taskList
-
-#Remove-Module psake -ErrorAction SilentlyContinue
+Remove-Module psake -ErrorAction SilentlyContinue
