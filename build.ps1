fa0eb50fa9ed83ff92652737ba5cf1516d8d127d
Brad Mitchell <bmitchell@boomtownroi.com>
2014-03-26 16:20:15 -0400

    Debug print

--- build.ps1
+++ build.ps1
@@ -9,6 +9,8 @@ $root = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 Import-Module $root\tools\psake\psake.psm1 -ErrorAction SilentlyContinue
 
+Write-Host "root: $root"
+
 Invoke-psake $root\default.ps1 -properties $properties -taskList $taskList
 
 Remove-Module psake -ErrorAction SilentlyContinue
